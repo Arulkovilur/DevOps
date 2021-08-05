e52c7723d02422dbe3a477cac4252a5884b14a9e
jockey <arul.s@HPY-CHE-LT-0231.Hitachi-payments.com>
2021-08-05 11:42:27 +0530

    First commit in Eclipse to GitHub

---
+++ Corejava/src/Coretraining/Helloworld.java
@@ -0,0 +1,22 @@
+package Coretraining;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Helloworld\n");
+		// System.out.printf("\n");
+		// System.out.printf("My name is %s", "Arul\n");
+		int a=10, b=15;
+		float x = 35.634f;
+		int c = a+b;
+		System.out.printf(a + "\n");
+		System.out.printf(b + "\n");
+		System.out.printf(c + "\n");
+		System.out.printf(x + "\n\n");
+		
+		System.out.printf("Formated output:\n");
+		System.out.printf("%5d\n%5d\n%5d\n%10.2f",a,b,c,x);
+	}
+
+}
